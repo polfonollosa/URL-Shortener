2f632299b39537903717c6b4414e3bd0987b026b
Pol Fonollosa Grandio <pfonollosag@edu.tecnocampus.cat>
2025-06-23 02:04:16 +0200

    SideBar functional

--- myapp/src/lib/SideBar.svelte
+++ myapp/src/lib/SideBar.svelte
@@ -1,5 +1,8 @@
 <div class="container">
-    <div class="sidebar">
+    <div class="sidebar active">
+        <div class="menu-btn">
+            <i class="fi fi-rr-angle-left"></i>
+        </div>
         <div class="head">
             <div class="user-img">
                 <img src="/AvatarLogo.jpg" alt=""/>
@@ -22,8 +25,8 @@
                     <li class="">
                         <a href="#">
                             <i class="fi fi-rr-user"></i>
-                            <span class="text">Your URL</span>
-                            <i class="fi fi-rr-angle-down arrow"></i>
+                            <span class="text">YourURL</span>
+                            <i class="fi fi-rr-angle-down "></i>
                         </a>
                         <ul class="sub-menu">
                             <li>
@@ -50,11 +53,11 @@
                             <span class="text">Schedules</span>
                         </a>
                     </li>
-                    <li class="">
+                    <li class="active">
                         <a href="#">
                             <i class="fi fi-rr-chart-line-up"></i>
-                            <span class="text">Your URL</span>
-                            <i class="fi fi-rr-angle-down arrow"></i>
+                            <span class="text">Statistics</span>
+                            <i class="fi fi-rr-angle-down"></i>
                         </a>
                         <ul class="sub-menu">
                             <li>
@@ -71,6 +74,34 @@
                     </li>
                 </ul>
             </div>
+            <div class="menu">
+                <p class="title">Settings</p>
+                <ul>
+                    <li class="">
+                        <a href="#">
+                            <i class="fi fi-rr-settings"></i>
+                            <span class="text">Settings</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="menu">
+            <p class="title">Account</p>
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fi fi-rr-question "></i>
+                        <span class="text">Help</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fi fi-rr-exit"></i>
+                        <span class="text">Logout</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
@@ -88,6 +119,7 @@
     }
 
     .sidebar{
+        position: relative;
         width: 256px;
         height: 100vh;
         display: flex;
@@ -96,6 +128,7 @@
         background-color: #fff;
         padding: 24px;
         border-radius: 30px;
+        transition: all 0.3s;
     }
 
     .sidebar .head{
@@ -139,6 +172,7 @@
     }
 
     .menu ul li{
+        position:relative;
         list-style: none;
         margin-bottom: 8px;
     }
@@ -164,6 +198,10 @@
     .menu ul li.active > a{
         color: #000;
         background-color: #f6f6f6;
+    }
+
+    .menu ul li.active fi.fi-rr-angle-down{
+        transform: rotate(180deg);
     }
 
     .menu ul li .fi.fi-rr-home{
@@ -201,13 +239,118 @@
         font-size: 13px;
     }
 
-    .arrow {
-        transition: transform 0.3s ease;
+    .active .fi.fi-rr-angle-down{
+        transform: rotate(180deg);
+    }
+    .menu:not(last-child){
+        padding-bottom: 5px;
+        margin-bottom: 5px;
+        border-bottom: 2px solid #f6f6f6;
     }
 
-    .arrow.rotated {
-        transform: rotate(-180deg);
+    .menu:not(last-child) .fi.fi-rr-settings{
+        font-size: 15px;
     }
+
+    .nav{
+        flex: 1;
+    }
+
+    .menu-btn{
+        position: absolute;
+        right: -14px;
+        top: 3.5%;
+        width: 28px;
+        height: 28px;
+        border-radius: 8px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #757575;
+        border: 3px solid #f6f6f6;
+        cursor: pointer;
+        background-color: #fff;
+    }
+
+    .menu-btn:hover i{
+        color: #000;
+    }
+
+    .menu-btn i{
+        transition: all 0.3s;
+    }
+
+
+    .sidebar.active{
+        width: 80px;
+    }
+    .sidebar.active .menu-btn i{
+        transform: rotate(180deg);
+    }
+
+    .sidebar.active .user-details{
+        display: none;
+    }
+
+    .sidebar.active .nav .menu .title {
+        text-align: center;
+    }
+
+    .sidebar.active .menu ul li .fi.fi-rr-angle-down{
+        display: none;
+    }
+
+    .sidebar.active .menu > ul > li > a{
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .sidebar.active .menu > ul > li > a .text{
+        position: absolute;
+        left: 70px;
+        top: 50%;
+        transform: translateY(-50%);
+        padding: 10px;
+        border-radius: 4px;
+        color: #fff;
+        background-color: #000;
+        opacity: 0;
+        visibility: hidden;
+        transition: all 0.3s;
+    }
+
+    .sidebar.active .menu > ul > li > a .text::after{
+        content: "";
+        position: absolute;
+        left: -5px;
+        top: 20%;
+        width: 20px;
+        height: 20px;
+        border-radius: 2px;
+        background-color: #000;
+        transform: rotate(450deg);
+        z-index: -1;
+    }
+
+    .sidebar.active .menu > ul > li > a:hover .text{
+        left: 50px;
+        opacity: 1;
+        visibility: visible;
+    }
+
+    .sidebar.active .menu .sub-menu{
+        position: absolute;
+        top: 0px;
+        left: 20px;
+        border-radius:20px;
+        padding: 10px 20px;
+        border: 1px solid #f6f6f6;
+        background-color: #fff;
+        box-shadow: 0px 10px 8px rgba(0,0,0,0.1);
+    }
+
 
 
 </style>
@@ -220,11 +363,16 @@
     onMount(() => {
         jQuery('.menu > ul > li').on('click', function () {
             jQuery(this).siblings().removeClass("active")
-            jQuery(this).toggleClass('active');
+            jQuery(this).toggleClass("active");
             jQuery(this).find("ul").slideToggle();
-            jQuery(this).siblings().find('ul').slideUp();
-            jQuery(this).siblings().find('ul').find('li').removeClass('active');
-            jQuery(this).find('.arrow').toggleClass('rotated');
+            jQuery(this).siblings().find("ul").slideUp();
+            jQuery(this).siblings().find("active").find("li").removeClass("active");
+        });
+    });
+
+    onMount(() => {
+        jQuery(".menu-btn").on('click', function () {
+            jQuery(".sidebar").toggleClass("active");
         });
     });
 
